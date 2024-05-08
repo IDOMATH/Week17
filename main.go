68d591606365bbe6829887048bb854742a9eec18
IDOMATH <btdothemath@gmail.com>
2024-05-07 22:19:12 -0500

    Adding wrapper around one endpoint

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	router := http.NewServeMux()
 
+	memstore := make(map[string]string)
+
 	router.HandleFunc("POST /mongo", handlePostMongo)
 	router.HandleFunc("POST /memstore", handlePostMemstore)
-	router.HandleFunc("GET /memstore", handleGetMemstore)
+	router.HandleFunc("GET /memstore", handleGetMemstore(memstore))
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
 
 func handlePostMongo(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +33,10 @@ func handlePostMemstore(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Posting to memstore"))
 }
 
-func handleGetMemstore(w http.ResponseWriter, r *http.Request) {
-	key := r.URL.Query().Get("key")
-	w.Write([]byte(fmt.Sprintf("key: %s", key)))
+func handleGetMemstore(memstore map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		value := memstore[key]
+		w.Write([]byte(fmt.Sprintf("key: %s value: %s", key, value)))
+	}
 }
