ef9f9323cb59b26412deb02b5cb8747081fbef3d
IDOMATH <btdothemath@gmail.com>
2024-05-10 22:45:51 -0500

    Setting up post memstore

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type memObj struct {
+	key   string `json:"key"`
+	value string `json:"value"`
+}
 
 func main() {
 	router := http.NewServeMux()
@@ -12,7 +18,7 @@ func main() {
 	memstore := make(map[string]string)
 
 	router.HandleFunc("POST /mongo", handlePostMongo)
-	router.HandleFunc("POST /memstore", handlePostMemstore)
+	router.HandleFunc("POST /memstore", handlePostMemstore(memstore))
 	router.HandleFunc("GET /memstore", handleGetMemstore(memstore))
 
 	server := http.Server{
@@ -28,9 +34,17 @@ func handlePostMongo(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Posting to Mongo"))
 }
 
-func handlePostMemstore(w http.ResponseWriter, r *http.Request) {
+func handlePostMemstore(memstore map[string]string) http.HandlerFunc {
 	// form fields are key, value
-	w.Write([]byte("Posting to memstore"))
+	return func(w http.ResponseWriter, r *http.Request) {
+		var obj memObj
+		err := json.NewDecoder(r.Body).Decode(&obj)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Write([]byte(fmt.Sprintf("key: %s, value: %s", obj.key, obj.value)))
+	}
 }
 
 func handleGetMemstore(memstore map[string]string) http.HandlerFunc {
